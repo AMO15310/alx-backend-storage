28fbda942a05a8081b44c10761952d9c4a1f5f00
AMO15310 <wachiraamos402@gmail.com>
2023-10-16 21:31:34 +0300

    task:find specific topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Manipulte mongo db using pymongo"""
+
+def schools_by_topic(mongo_collection,topic):
+    """Return a list of content with specific topic"""
+    return [mongo_collection.find("topics":topic)]
