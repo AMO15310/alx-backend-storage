b01a5ab1dbbf30cd7cb42037fcd92939d6cb9005
AMO15310 <wachiraamos402@gmail.com>
2023-10-16 21:36:08 +0300

    task:find specific topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -3,5 +3,4 @@
 
 def schools_by_topic(mongo_collection,topic):
     """Return a list of content with specific topic"""
-    documents =  list(mongo_collection.find("topics":topic))
-    return documents
+    return[mongo_collection.find({"topics":topic})]
